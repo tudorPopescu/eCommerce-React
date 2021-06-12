705584b4baa2c589f93d43980a93c30dde07a284
tudorPopescu <tudorp2@gmail.com>
2021-06-12 14:42:22 +0300

    worked on collection page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,16 @@
-import CollectionOverview from "../../components/collection-overview/collection-overview.component";
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-const ShopPage = () => (
-  <div>
-    <CollectionOverview />
-  </div>
-);
+import CollectionOverview from "../../components/collection-overview/collection-overview.component";
+import Collection from '../collection/collection.component';
+
+const ShopPage = ({ match }) => {
+  return(
+    <div>
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route path={`${match.path}/:id_collection`} component={Collection} />
+    </div>
+  );
+}
 
 export default ShopPage;
